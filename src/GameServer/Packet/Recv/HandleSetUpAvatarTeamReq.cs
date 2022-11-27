6bc1a78830dea750d8aad85d68cf8827f828032f
SwetyCore <swety2003@outlook.com>
2022-11-27 22:41:10 +0800

    Handled SetUpAvatarTeamReq packet

---
+++ src/GameServer/Packet/Recv/HandleSetUpAvatarTeamReq.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weedwacker.GameServer.Enums;
+using Weedwacker.Shared.Network.Proto;
+
+namespace Weedwacker.GameServer.Packet.Recv
+{
+    [OpCode((ushort)OpCode.SetUpAvatarTeamReq)]
+    internal class HandleSetUpAvatarTeamReq : BaseHandler
+    {
+        public override async Task HandleAsync(Connection session, byte[] header, byte[] payload)
+        {
+            SetUpAvatarTeamReq proto = SetUpAvatarTeamReq.Parser.ParseFrom(payload);
+            
+            await session.Player.TeamManager.SetupAvatarTeamAsync((int)proto.TeamId, proto.AvatarTeamGuidList.Select(x=>(long)x).ToList());
+        }
+    }
+}
